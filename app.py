94432013eec3f99fc2d0913bdaed4a8e5f555d6c
goat-scape <macabre.marx@yandex.ru>
2022-04-04 03:52:40 -0500

    sheeeesh

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from utils.set_bot_commands import set_default_commands
-
+import random
+import time
+import threading
 
 async def on_startup(dp):
     import filters
@@ -11,10 +13,21 @@ async def on_startup(dp):
     await on_startup_notify(dp)
     await set_default_commands(dp)
 
+def thread_function():
+    while True:
+        try:
+            randomic = random.randint(0,10)
+            print(randomic)
+            time.sleep(1200)
+        except:
+            pass
+
 
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
-
+    x1 = threading.Thread(target=thread_function, args=())
+    x1.start()
     executor.start_polling(dp, on_startup=on_startup)
+
 
