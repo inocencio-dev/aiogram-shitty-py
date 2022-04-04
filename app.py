bbb228e56df8122280ae139746dc57e36ca5a672
goat-scape <macabre.marx@yandex.ru>
2022-04-04 07:17:37 -0500

    pussy

--- app.py
+++ app.py
@@ -2,16 +2,36 @@ from utils.set_bot_commands import set_default_commands
 import random
 import time
 import threading
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils.executor import start_webhook
+from data import config
 
-async def on_startup(dp):
+HEROKU_APP_NAME = config.HEROKU_APP_NAME
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{config.BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = config.PORT
+
+
+async def on_startup(dispatcher):
     import filters
     import middlewares
-    filters.setup(dp)
-    middlewares.setup(dp)
-
+    filters.setup(dispatcher)
+    middlewares.setup(dispatcher)
     from utils.notify_admins import on_startup_notify
-    await on_startup_notify(dp)
-    await set_default_commands(dp)
+    await on_startup_notify(dispatcher)
+    await set_default_commands(dispatcher)
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await dispatcher.bot.delete_webhook()
+
 
 def thread_function():
     while True:
@@ -22,12 +42,16 @@ def thread_function():
         except:
             pass
 
-
 if __name__ == '__main__':
-    from aiogram import executor
     from handlers import dp
     x1 = threading.Thread(target=thread_function, args=())
     x1.start()
-    executor.start_polling(dp, on_startup=on_startup)
-
-
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
--- data/config.py
+++ data/config.py
@@ -7,4 +7,5 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
-
+HEROKU_APP_NAME = env.str("HEROKU_APP_NAME")
+PORT=env.str("PORT")
